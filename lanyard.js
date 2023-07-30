7aeea9111da90d30753d930fe6763274cb1733c9
fq0e <90973963+fq0e@users.noreply.github.com>
2023-07-30 15:00:14 +0000

    Spotify

--- lanyard.js
+++ lanyard.js
@@ -1,6 +1,7 @@
 const colors = {
     offline: "#f2d9ed",
     online: "#57a55a",
+    idle: "#ffcc00",
     dnd: "#bf5752"
 }
 
@@ -14,9 +15,26 @@ setTimeout(() => {
     }
     
     const req = httpGet("https://api.lanyard.rest/v1/users/380831451559493632")
-    const status = JSON.parse(req).data.discord_status.toUpperCase()
+    const data = JSON.parse(req).data
+    const status = data.discord_status.toUpperCase()
+    const listening = data.listening_to_spotify
     const color = colors[status.toLowerCase()]
-    console.log(color)
 
-    document.getElementById("discordStatus").innerHTML = `currently <span style="color: ${color}">${status}</span> on discord.`
+    document.getElementById("discordStatus").innerHTML = `currently <span style="color: ${color}">${status}</span> on discord`
+
+    if (listening  == true) {
+        const album = data.spotify.album
+        const artist = data.spotify.artist
+        const song = data.spotify.song
+
+        const album_art = data.spotify.album_art_url
+
+        document.getElementById("spotifyStatus").innerHTML = `listening to ${song} <br> by ${artist}`
+
+        document.getElementById("brah").style.display = "inline"
+        document.getElementById("spotifyAlbum").style.display = "inline"
+
+        document.getElementById("spotifyImage").src = album_art
+        document.getElementById("albumName").innerHTML = album
+    }
 }, 0)
